b083ede3285b9bde245a20580eb2fdf1e0d9d3a1
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2020-09-02 04:33:12 +0530

    Added HTML View

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ emails = e['Emails'].values
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(SenderAddress, password)
-msg = "Hello this is a email form python"
+#msg = "Hello this is a email form python"
+msg= """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
 subject = "Hello world"
 body = "Subject: {}\n\n{}".format(subject,msg)
 for email in emails:
